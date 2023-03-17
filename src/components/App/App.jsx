1ecd4b1d48a94feb5a7c3d3055d2b6cfcd7779f4
Everett Butler <everettjamesbutler@gmail.com>
2023-03-17 14:45:42 -0500

    got my header component finished and exported! Working on the other componenets now. Then I will get the POST, UPDATE, and DELETE functionalities working!

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import {useState} from 'react';
+import './App.css';
+import Header from '../Header/Header.jsx';
 
 function App () {
   
   return (
     <div>
-      <h1>TO DO APP</h1>
+      <Header />
     </div>
   );
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header>
+            <h1>Everett's Exceptional To-Do List</h1>
+        </header>
+    );
+}
+
+export default Header;
